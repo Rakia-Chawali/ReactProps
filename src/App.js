ab7821f7b5919b76579fbe0fbd6ad517dc939206
Rakia-Chawali <rakiamohamed117@gmail.com>
2022-12-05 02:41:25 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import ProfileList from './components/ProfileList';
+import React, { Component } from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+const myStyle = {
+  backgroundImage: "url('https://wallpaperaccess.com/full/270156.jpg')",
+  height: "100vh",
+  
+  
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+};
+
+  return (
+     <div style={myStyle}>
+         <ProfileList />
+          <footer  > Copyright 2022 all rights reserved</footer>
+      </div>
+   
+ ) };
 export default App;
